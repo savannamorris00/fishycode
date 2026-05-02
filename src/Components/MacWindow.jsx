930ca91bb5305e01e13211abb7ff64279f789e13
Savanna Morris <savanna.morris00@gmail.com>
2026-05-02 14:28:28 -0500

    Added divider

--- src/Components/MacWindow.jsx
+++ src/Components/MacWindow.jsx
@@ -206,7 +206,7 @@ export default function MacWindow() {
                   <li><b>Dean's List</b> – Spring 2025, Fall 2025</li>
                 </ul>
               </div>
-
+                <div className="border-t border-gray-300 my-4"></div>
               <div>
                 <h2 className="font-bold text-lg mb-2">Current Employment</h2>
                 <div className="mb-4">
